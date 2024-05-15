2f5403bfd43f4978bfcff9ee4bb55790ced3da3e
Mateus Nicoletti <m.nico15@hotmail.com>
2024-05-15 20:44:19 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nk = new Nokia("11963611244", "E63", "10851685456", 128);
+Iphone ip = new Iphone("11975199687", "iPhone 11 Pro", "10185454015", 256);
+
+
+Console.WriteLine("Nokia E63:");
+nk.Ligar();
+nk.InstalarAplicativo("Whatsapp");
+nk.ReceberLigacao();
+
+Console.WriteLine("iPhone:");
+ip.Ligar();
+ip.InstalarAplicativo("Telegram");
+ip.ReceberLigacao();
